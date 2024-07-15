f296e1ef9f411ed1dde7e61d24f23d324c881f6e
HamzaReko <hamzamgplay123@gmail.com>
2024-07-15 21:19:10 +0300

    Final

--- Js/color-modes.js
+++ Js/color-modes.js
@@ -7,8 +7,8 @@
 (() => {
   'use strict'
 
-  const getStoredTheme = () => localStorage.getItem('theme')
-  const setStoredTheme = theme => localStorage.setItem('theme', theme)
+  const getStoredTheme = () => localStorage.getItem('theme1')
+  const setStoredTheme = theme => localStorage.setItem('theme1', theme)
 
   const getPreferredTheme = () => {
     const storedTheme = getStoredTheme()
